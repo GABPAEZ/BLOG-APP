a67a77bd622840da7c6e1d47503a5d7846464fff
Gabriel Paez <gabpaez@gmail.com>
2020-03-08 11:52:34 -0300

    Se agrego elimnar Blog y su funcionalidad

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,19 @@
 import createDateContext from "./createDataContext";
-
+//import "react-native-get-random-values";
+//import {v4 as uuidv4} from "uuid"; ---NO ME FUNCIONA NO SE PORQUE
 // Reducer recibe el state=blogPosts y un action
+
+
 
 const blogsReducer = (state, actions) => {
   switch (actions.type) {
+    case 'DELETE_POST':
+      return state.filter((item) => item.id !== actions.payload)
     case "ADD_BLOG_POST":
-      return [...state, { title: `My Post #${state.length + 1}` }];
+      return [
+        ...state,
+        {id: `id # ${Math.floor(Math.random()*99999)}` , title: `My Post #${state.length + 1}` }
+      ];
     default:
       return state;
   }
@@ -16,11 +24,18 @@ const blogsReducer = (state, actions) => {
 const addBlogPost = dispatch => {
   return () => {
     dispatch({ type: "ADD_BLOG_POST" });
+  };
+};
+
+const deletePost = dispatch => {
+  return (id) => {
+    dispatch({type: 'DELETE_POST', payload: id})
   }
 };
 
+
 export const { Context, Provider } = createDateContext(
   blogsReducer,
-  { addBlogPost },
+  { addBlogPost, deletePost },
   []
 );
--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,26 +1,59 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList, Button } from "react-native";
-import { Context }from "../context/BlogContext";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity
+} from "react-native";
+import { Context } from "../context/BlogContext";
+import { Entypo } from "@expo/vector-icons";
 
 const indexScreen = () => {
   //llamamos al valor value pasado en el context para mostrarlo en esta pantalla
-  const { state, addBlogPost } = useContext(Context);
+  const { state, addBlogPost, deletePost } = useContext(Context);
 
   return (
     <View>
       {/* <Text>Index Screen Page</Text> */}
       <Button title="Add Blog Post" onPress={addBlogPost} />
+
       <FlatList
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.text}>
+                {item.title} - {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => deletePost(item.id)}>
+                <Entypo style={styles.avatar} name="trash" />
+              </TouchableOpacity>
+            </View>
+          );
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc"
+  },
+  text: {
+    fontSize: 18
+  },
+  avatar: {
+    fontSize: 18
+  }
+});
 
 export default indexScreen;
